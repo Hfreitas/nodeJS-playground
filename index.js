9124b17d90042702ce8457074b3d01e49ac2a9c6
Hfreitas <hfreitas86@gmail.com>
2020-08-14 17:35:06 -0300

    ok

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ const marvel = './io-local/myMarvelous.txt';
 const otaku = './io-local/myOtaku.txt';
 const coach = './io-local/myCoach.txt';
 
-readFile
-  .readFilePromise(marvel)
+readFile(marvel)
   .then((content) => {
     console.log(`Lido arquivo 1 com ${content.byteLength} bytes`);
-    return readFile.readFilePromise(otaku);
+    return readFile(otaku);
   })
   .then((content) => {
     console.log(`Lido arquivo 2 com ${content.byteLength} bytes`);
-    return readFile.readFilePromise(coach);
+    return readFile(coach);
   })
   .then((content) => {
     console.log(`Lido arquivo 3 com ${content.byteLength} bytes`);
--- io-local/readFile.js
+++ io-local/readFile.js
@@ -1,14 +1,11 @@
 const fs = require('fs');
-const { rejects } = require('assert');
 
 // readFile é um método assíncrono, recebe o arquivo a ser lido, string/encoding e uma callback node-style(recebe error, result);
 
-function readFilePromise(file) {
+module.exports = function (file) {
   return new Promise((resolve, reject) => {
     fs.readFile(file, (error, content) => {
-      return err ? reject(error) : resolve(content);
+      return error ? reject(error) : resolve(content);
     });
   });
 }
-
-export default readFilePromise;
