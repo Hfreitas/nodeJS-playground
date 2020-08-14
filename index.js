37789b857b139d817176b256b27f59866ecdcdc6
Hfreitas <hfreitas86@gmail.com>
2020-08-14 17:22:17 -0300

    readFile promise

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// make async again!!!!
+const readFile = require('./io-local/readFile');
+
+const marvel = './io-local/myMarvelous.txt';
+const otaku = './io-local/myOtaku.txt';
+const coach = './io-local/myCoach.txt';
+
+readFile
+  .readFilePromise(marvel)
+  .then((content) => {
+    console.log(`Lido arquivo 1 com ${content.byteLength} bytes`);
+    return readFile.readFilePromise(otaku);
+  })
+  .then((content) => {
+    console.log(`Lido arquivo 2 com ${content.byteLength} bytes`);
+    return readFile.readFilePromise(coach);
+  })
+  .then((content) => {
+    console.log(`Lido arquivo 3 com ${content.byteLength} bytes`);
+  })
+  .catch((error) => {
+    console.log(`Erro ao ler arquivos: ${error.message}`);
+  });
--- io-local/readFile.js
+++ io-local/readFile.js
@@ -1,15 +1,14 @@
 const fs = require('fs');
+const { rejects } = require('assert');
 
 // readFile é um método assíncrono, recebe o arquivo a ser lido, string/encoding e uma callback node-style(recebe error, result);
 
-const archiveName = 'myMarvelous.txt';
+function readFilePromise(file) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (error, content) => {
+      return err ? reject(error) : resolve(content);
+    });
+  });
+}
 
-fs.readFile(archiveName, (error, data) => {
-  if (error) {
-    console.error(
-      `Não foi possível ler o arquivo ${archiveName}\n Erro: ${error.message}`,
-    );
-    return;
-  }
-  console.log(`Sucesso! Conteúdo do arquivo: ${data.toString('utf-8')}`);
-});
+export default readFilePromise;
