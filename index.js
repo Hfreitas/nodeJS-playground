9b5eed2c39d6685633c1cf537e6d8decd864f93b
Hfreitas <hfreitas86@gmail.com>
2020-08-13 22:50:49 -0300

    factorial ok

--- index.js
+++ index.js
@@ -3,50 +3,31 @@ const inquirer = require('inquirer');
 function VALIDATE_NUMBER(input) {
   const parsedInput = parseFloat(input);
   // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/isNaN
-  const checkInput = !isNaN(parsedInput) || 'Digite um valor válido';
+  const checkInput =
+    (!isNaN(parsedInput) && Number.isInteger(parsedInput)) ||
+    'Digite um valor válido';
   return checkInput;
 }
 
-const IMC_CALC = (height, weight) =>
-  Math.abs(weight / Math.pow(height, 2)).toFixed(2);
-
-const IMC_AVALIATOR = (imc) => {
-  // https://stackoverflow.com/questions/2696436/switch-case-with-conditions
-  switch (true) {
-    case imc < 18.5:
-      return `${imc} - Abaixo do peso (magreza)`;
-    case imc >= 18.5 && imc <= 24.99:
-      return `${imc} - Peso normal`;
-    case imc >= 25 && imc <= 29.99:
-      return `${imc} - Acima do peso (sobrepeso)`;
-    case imc >= 30 && imc <= 34.99:
-      return `${imc} - Obesidade grau I`;
-    case imc >= 35 && imc <= 39.99:
-      return `${imc} - Obesidade grau II`;
-    default:
-      return `${imc} - Obesidade graus III e IV`;
-  }
+const FACTORIAL = (num) => {
+  if (num < 0) return -1;
+  if (num == 0) return 1;
+  return num * FACTORIAL(num - 1);
 };
 
 const READ_DATA = async () => {
   // https://www.digitalocean.com/community/tutorials/nodejs-interactive-command-line-prompts
-  const answers = await inquirer.prompt([
+  const answer = await inquirer.prompt([
     {
-      name: 'height',
+      name: 'number',
       type: 'input',
-      message: 'Digite sua altura (com duas casas decimais): ',
-      validate: VALIDATE_NUMBER,
-    },
-    {
-      name: 'weight',
-      type: 'input',
-      message: 'Digite seu peso (com duas casa decimais): ',
+      message: 'Digite um número inteiro ',
       validate: VALIDATE_NUMBER,
     },
   ]);
-  return console.log(IMC_AVALIATOR(
-    IMC_CALC(parseFloat(answers.height), parseFloat(answers.weight)),
-  ));
+  return console.log(
+    FACTORIAL(answer.number)
+  );
 };
 
 READ_DATA();
