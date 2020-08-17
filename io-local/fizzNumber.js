914770e18afb0f0647686ae54c339dbdb7e59477
Hfreitas <hfreitas86@gmail.com>
2020-08-17 14:35:01 -0300

    faz o solicitado, mas preciso de revisão

--- io-local/fizzNumber.js
+++ io-local/fizzNumber.js
@@ -1,8 +1,23 @@
+// Adaptei desta solução https://medium.com/dailyjs/rewriting-javascript-converting-an-array-of-objects-to-an-object-ec579cafbfc7
+
+const parseParams = (...params) => {
+  const arrayParams = params.reduce((acc, param) => {
+    isNaN(param) ? (acc[param] = parseInt(param, 10)) : (acc[param] = param);
+    return acc;
+  }, {});
+  return arrayParams;
+};
+
 module.exports = async (x, y, z) => {
+  const data = parseParams(x, y, z);
   return new Promise((resolve, reject) => {
-    if (typeof x !== 'number' || typeof y !== 'number' || typeof z !== 'number')
+    console.log(data);
+    if (
+      isNaN(data[x]) ||
+      isNaN(data[y]) ||
+      isNaN(data[z])
+    )
       return reject(new Error('Jovem, apenas números, por favor...'));
-      console.log(typeof x)
     return resolve(x + y);
   }).then((sum) => {
     return sum * z <= 50
